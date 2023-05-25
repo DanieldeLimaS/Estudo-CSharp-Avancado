2f81a69e0009a987f53b6c909bcf2e35f2540501
DanieldelimaS <danielsuporteti@outlook.com.br>
2023-05-24 23:12:38 -0300

    Primeira implementacao, Serealizar e deserializar

---
+++ 00_Biblioteca/Usuario.cs
@@ -0,0 +1,9 @@
+﻿namespace _00_Biblioteca
+{
+    public class Usuario
+    {
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ 01_SerializarXML/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using _00_Biblioteca;
+using System.Xml.Serialization;
+
+Usuario user = new Usuario()
+{
+    Nome = "Daniel de Lima",
+    CPF = "123.123.123-12",
+    Email = "meuemail@gmail.com"
+};
+XmlSerializer serializador = new XmlSerializer(typeof(Usuario));
+StreamWriter strem = new StreamWriter(@"D:\Documents\dev\Course\Estudo-CSharp-Avancado\01_SerializarXML.xml");
+serializador.Serialize(strem, user);
